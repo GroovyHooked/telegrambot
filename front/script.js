cc5338072b3c7325b2b88cafbfcab889f3b6b777
GroovyHooked <dafrenchie2002@yahoo.fr>
2024-02-04 09:20:13 +0000

    Refacto

--- front/script.js
+++ front/script.js
@@ -3,37 +3,33 @@ const updateQuantityBtn = document.querySelector('.btn.btn-outline-primary')
 const dropdownBtn = document.querySelector('.btn-dropdown')
 const assetDropDownBtn = document.querySelector('#assets')
 
-let initialAsset = 'bitcoin'
+let chartAsset = 'bitcoin'
 let intervalId;
 
 if (updateQuantityBtn) updateQuantityBtn.addEventListener('click', sendQuantityToServer)
 if (dropdownBtn) dropdownBtn.addEventListener('click', toggleDropdown)
 if (assetDropDownBtn) {
     assetDropDownBtn.addEventListener('change', function () {
-        requestGraphDataFromServer({ asset: assetDropDownBtn.value })
+        updateGraphConttroller({ asset: assetDropDownBtn.value })
     })
 }
 
-if (assetDropDownBtn) updateGraphConttroller({ asset: initialAsset })
+if (assetDropDownBtn) updateGraphConttroller({ asset: chartAsset })
 
-function requestGraphDataFromServer(asset) {
-    fetch('/graphdata', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(asset),
-    })
-        .then(response => response.json())
-        .then(data => {
-            initialAsset = data.asset
-            if (assetDropDownBtn) updateGraphConttroller({ asset: initialAsset })
-
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
-}
+// function requestGraphDataFromServer(asset) {
+//     // console.log(asset);
+//     // const response = await fetch('/graphdata', {
+//     //     method: 'POST',
+//     //     headers: {
+//     //         'Content-Type': 'application/json',
+//     //     },
+//     //     body: JSON.stringify(asset),
+//     // })
+//     // const data = await response.json();
+//     // chartAsset = data.asset
+//     console.log(asset.asset);
+//     if (assetDropDownBtn) updateGraphConttroller({ asset: asset.asset })
+// }
 
 function updateGraphOnHomePage(assetName) {
     const img = document.querySelector('.img-fluid')
@@ -58,38 +54,29 @@ function retrieveQuantitiesFromInputs() {
     return inputValues;
 }
 
-function sendQuantityToServer() {
+async function sendQuantityToServer() {
     const quantities = retrieveQuantitiesFromInputs()
-    fetch("http://192.168.1.111/updatequantities", {
+    const response = await fetch("http://192.168.1.111/updatequantities", {
         method: "POST",
         headers: {
             "Content-Type": "application/json",
         },
         body: JSON.stringify(quantities),
     })
-        .then((response) => {
-            if (response.ok) {
-                const successAlert = document.querySelector(".alert.alert-success");
-                successAlert.classList.remove("d-none");
-                setTimeout(() => {
-                    successAlert.classList.add("d-none");
-                }, 2000);
-            } else {
-                const errorAlert = document.querySelector(".alert.alert-danger");
-                errorAlert.classList.remove("d-none");
-                setTimeout(() => {
-                    errorAlert.classList.add("d-none");
-                }, 2000);
-            }
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-        });
+    response.ok ? displayAlert('.alert.alert-success') : displayAlert('.alert.alert-danger')
+}
+
+function displayAlert(className) {
+    const alert = document.querySelector(className)
+    alert.classList.remove('d-none')
+    setTimeout(() => {
+        alert.classList.add('d-none')
+    }, 2000)
 }
 
 function toggleDropdown() {
     const dropdown = document.querySelector('.menu-dropdown')
-    dropdown.classList.toggle('show')
+    dropdown.classList.toggle('display-dropdown')
 }
 
 const eventSource = new EventSource('/total');
@@ -97,7 +84,17 @@ const eventSource = new EventSource('/total');
 eventSource.onmessage = (event) => {
     const { total } = JSON.parse(event.data);
     const totalNode = document.querySelector('.total-balance-value')
-    if (totalNode) totalNode.textContent = `${total}€`
+    const totalPortfolioNode = document.querySelector('.total-portfolio-value')
+    if (totalNode) {
+        totalNode.classList.add('updated-value')
+        totalNode.textContent = `${total}€`
+        setTimeout(() => {
+            totalNode.classList.remove('updated-value')
+        }, 100)
+    }
+    if (totalPortfolioNode) {
+        totalPortfolioNode.textContent = `${total}€`
+    }
 };
 
 eventSource.onerror = function (error) {
